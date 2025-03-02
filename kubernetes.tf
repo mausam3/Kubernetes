31a7fb538193c72138de296b5e4d0b8c37e12140
mausam3 <161200469+mausam3@users.noreply.github.com>
2025-03-02 13:56:59 +0530

    Update3 kubernetes.tf

--- kubernetes.tf
+++ kubernetes.tf
@@ -1,43 +1,96 @@
-terraform {
-  required_providers {
-    google = {
-      source  = "hashicorp/google"
-      version = "3.52.0"
-    }
-    kubernetes = {
-      source  = "hashicorp/kubernetes"
-      version = ">= 2.0.1"
-    }
-  }
-}
-
-data "terraform_remote_state" "gke" {
-  backend = "local"
-
-  config = {
-    path = "../learn-terraform-provision-gke-cluster/terraform.tfstate"
-  }
-}
-
-# Retrieve GKE cluster information
+# Define provider to use Google Cloud
 provider "google" {
-  project = data.terraform_remote_state.gke.outputs.project_id
-  region  = data.terraform_remote_state.gke.outputs.region
+  project = "mausam-pandey"       # Replace with your GCP project ID
+  region  = "us-central1"           # Replace with your desired region
+  zone    = "us-central1-c"         # Replace with your desired zone
 }
 
-# Configure kubernetes provider with Oauth2 access token.
-# https://registry.terraform.io/providers/hashicorp/google/latest/docs/data-sources/client_config
-# This fetches a new token, which will expire in 1 hour.
-data "google_client_config" "default" {}
 
-data "google_container_cluster" "my_cluster" {
-  name     = data.terraform_remote_state.gke.outputs.kubernetes_cluster_name
-  location = data.terraform_remote_state.gke.outputs.region
+variable "region" {
+  description = "The region where GKE cluster will be created"
+  default     = "us-central1"
 }
 
-provider "kubernetes" {
-  host = data.terraform_remote_state.gke.outputs.kubernetes_cluster_host
+variable "cluster_name" {
+  description = "my-gke-cluster"
+  default     = "my-gke-cluster"
+}
 
-  token                  = data.google_client_config.default.access_token
-  cluster_ca_certificate = base64decode(data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate)
+variable "node_pool_name" {
+  description = "The name of the node pool"
+  default     = "default-node-pool"
+}
+
+variable "node_count" {
+  description = "The initial number of nodes in the node pool"
+  default     = 3
+}
+
+variable "node_machine_type" {
+  description = "Machine type for nodes in the node pool"
+  default     = "e2-medium"
+}
+
+variable "node_disk_size_gb" {
+  description = "Disk size (in GB) for each node"
+  default     = 100
+}
+
+variable "vpc_network" {
+  description = "The VPC network name"
+  default     = "default"
+}
+
+variable "subnet_name" {
+  description = "The subnet name"
+  default     = "default"
+}
+
+# Create the GKE cluster
+resource "google_container_cluster" "primary" {
+  name     = my-cluster
+  location = "us-central1-c"
+
+  initial_node_count = 3
+  node_version       = "latest"  # You can specify the exact version if you want
+
+  # Define the node pool configuration
+  node_config {
+    machine_type = var.node_machine_type
+    disk_size_gb = var.node_disk_size_gb
+    image_type   = "COS"  # Container Optimized OS (default for GKE)
+  }
+
+  # Network and Subnet configuration
+  network    = var.vpc_network
+  subnetwork = var.subnet_name
+
+  # Enable logging and monitoring
+  enable_logging        = true
+  enable_monitoring     = true
+  enable_network_policy = true
+
+  ip_allocation_policy {
+    use_ip_aliases = true  # Enable IP aliasing for pod networking
+  }
+
+  # Node pool configuration
+  node_pool {
+    name = var.node_pool_name
+
+    autoscaling {
+      min_node_count = 1
+      max_node_count = 5
+    }
+
+    management {
+      auto_upgrade = true
+      auto_repair  = true
+    }
+  }
+}
+
+# Output the kubeconfig for accessing the GKE cluster
+output "kubeconfig" {
+  value = google_container_cluster.primary.kube_config[0].raw_kube_config
 }
